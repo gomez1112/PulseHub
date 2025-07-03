eacea7f72a22c40fad65af62a7c95396ea2c8ab7
Gerard Gomez <gerardgomez11@me.comgerardgomez11@me.comgerardgomez11@me.com>
2025-07-03 12:00:35 -0400

    Initial Commit

---
+++ PulseHub/PulseHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  PulseHubApp.swift
+//  PulseHub
+//
+//  Created by Gerard Gomez on 7/3/25.
+//
+
+import SwiftUI
+
+@main
+struct PulseHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PulseHubTests/PulseHubTests.swift
@@ -0,0 +1,16 @@
+//
+//  PulseHubTests.swift
+//  PulseHubTests
+//
+//  Created by Gerard Gomez on 7/3/25.
+//
+
+import Testing
+
+struct PulseHubTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
